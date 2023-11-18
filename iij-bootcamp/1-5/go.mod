761ad7e34cf7c6a892d074ad8af8c238778d2f3f
seiya_tagami <bepozurapu274@gmail.com>
2023-11-18 22:43:57 +0900

    adding iij-bootcamp 1-5

---
+++ iij-bootcamp/1-5/go.mod
@@ -0,0 +1,3 @@
+module github.com/Seiya-Tagami/go-practice/iij-bootcamp/1-5
+
+go 1.21.0
---
+++ iij-bootcamp/1-5/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Seiya-Tagami/go-practice/iij-bootcamp/1-5/monkey"
+	"os"
+)
+
+func main() {
+	var name1 string = "GYUDON"
+	if _, err := monkey.Eat(name1); err != nil {
+		fmt.Fprintf(os.Stderr, "cannot eat: %s\n", err)
+	}
+
+	var name2 string = ""
+	if _, err := monkey.Eat(name2); err != nil {
+		fmt.Fprintf(os.Stderr, "cannot eat: %s\n", err)
+	}
+}
