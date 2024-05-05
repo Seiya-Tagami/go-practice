162c9d9fab56d611745383442b87a9b8052053d4
Seiya Tagami <bepozurapu274@gmail.com>
2024-05-05 07:48:48 +0000

    chapter-3 done

--- books/go-pratice-introduction/chapter-3/main.go
+++ books/go-pratice-introduction/chapter-3/main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"reflect"
+	"runtime"
 )
+
+// "handler function" chain example
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello!")
+}
+
+func log(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
+		fmt.Println("Handler function called - " + name)
+		h(w, r)
+	}
+}
+
+func protect(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		h(w, r)
+	}
+}
 
 func main() {
 	server := http.Server{
 		Addr:    "127.0.0.1:8080",
 		Handler: nil,
 	}
+	http.HandleFunc("/hello", protect(log(hello)))
 	server.ListenAndServe()
 }
