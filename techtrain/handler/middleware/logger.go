4f1cb56fe40dde8cdc7117d42d17bb85ba544ae8
taga3s <bepozurapu274@gmail.com>
2024-08-29 15:55:19 +0900

    fix: access logger

--- techtrain/handler/middleware/logger.go
+++ techtrain/handler/middleware/logger.go
@@ -1,11 +1,19 @@
 package middleware
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"techtrain-go-practice/context/os"
 	"time"
 )
+
+type Log struct {
+	Timestamp time.Time `json:"timestamp"`
+	Latency   int64     `json:"latency"`
+	Path      string    `json:"path"`
+	OS        string    `json:"os"`
+}
 
 func Logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -13,7 +21,7 @@ func Logger(next http.Handler) http.Handler {
 
 		next.ServeHTTP(w, r)
 
-		latency := time.Since(accessTime)
+		latency := time.Since(accessTime).Milliseconds()
 		path := r.URL.Path
 		os, err := os.OSFromContext(r.Context())
 		if err != nil {
@@ -21,6 +29,19 @@ func Logger(next http.Handler) http.Handler {
 			return
 		}
 
-		log.Printf(`{"timestamp": "%s", "latency": "%s", "path": "%s", "os": "%s"}`, time.Now(), latency, path, os)
+		accessLog := Log{
+			Timestamp: accessTime,
+			Latency:   latency,
+			Path:      path,
+			OS:        os,
+		}
+
+		bytes, err := json.Marshal(accessLog)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		log.Println(string(bytes))
 	})
 }
